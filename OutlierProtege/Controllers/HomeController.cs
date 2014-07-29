f547310e8af8b2c81baea9e80970a9d2cfb00124
goldnarms <arnstej@gmail.com>
2014-07-30 00:48:17 +0200

    WIP Started on frontpage

--- OutlierProtege/Controllers/HomeController.cs
+++ OutlierProtege/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace OutlierProtege.Controllers
 
         public ActionResult FrontPage()
         {
-            return PartialView()
+            return PartialView("../Template/FrontPage");
         }
     }
 }
